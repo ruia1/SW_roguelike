21d97dc6ae596ec14cb448bf99f69abb90b0a6cd
Asagegege <SiroKuro9191@gmail.com>
2023-06-15 18:04:45 +0900

    fourth commit

--- sw/map.py
+++ sw/map.py
@@ -23,49 +23,51 @@ def set_route(seed,flo,xx):
         print(9,end = "")
     else:
         if xx == 0:
-            if (seed + r) % 3 == 0:
+            if (seed + r) % 9 == 0 or (seed + r) % 9 == 3 or (seed + r) % 9 == 5 or (seed + r) % 9 == 7:
                 map[flo][xx].next_route1 = 1
                 print("a",end = "")
-            elif (seed + r) % 3 == 1:
+            elif (seed + r) % 9 == 1 or (seed + r) % 9 == 4 or (seed + r) % 9 == 6 or (seed + r) % 9 == 8:
                 map[flo][xx].next_route2 = 0
                 print("b",end = "")
-            elif (seed + r) % 3 == 2:
+            elif (seed + r) % 9 == 2:
                 map[flo][xx].next_route1 = 1
                 map[flo][xx].next_route2 = 0
                 print("c",end = "")
         elif xx == 5:
-            if (seed + r) % 3 == 0:
+            if (seed + r) % 9 == 0 or (seed + r) % 9 == 3 or (seed + r) % 9 == 5 or (seed + r) % 9 == 7:
                 map[flo][xx].next_route2 = 0
                 print("A",end = "")
-            elif (seed + r) % 3 == 1:
+            elif (seed + r) % 9 == 1 or (seed + r) % 9 == 4 or (seed + r) % 9 == 6 or (seed + r) % 9 == 8:
                 map[flo][xx].next_route3 = -1
                 print("B",end = "")
-            elif (seed + r) % 3 == 2:
+            elif (seed + r) % 9 == 2:
                 map[flo][xx].next_route2 = 0
                 map[flo][xx].next_route3 = -1
                 print("C",end = "")
         else:
-            if (seed + r) % 7 == 0:
+            if (seed + r) % 25 == 0:
+                map[flo][xx].next_route1 = 1
                 map[flo][xx].next_route2 = 0
+                map[flo][xx].next_route3 = -1
                 print(1,end = "")
-            elif (seed + r) % 7 == 1:
+            elif (seed + r) % 25 == 1 or (seed + r) % 25 == 7 or (seed + r) % 25 == 13 or (seed + r) % 25 == 16 or (seed + r) % 25 == 19 or (seed + r) % 25 == 22:
                 map[flo][xx].next_route1 = 1
                 print(2,end = "")
-            elif (seed + r) % 7 == 2:
+            elif (seed + r) % 25 == 2 or (seed + r) % 25 == 8 or (seed + r) % 25 == 14 or (seed + r) % 25 == 17 or (seed + r) % 25 == 20 or (seed + r) % 25 == 23:
                 map[flo][xx].next_route2 = 0
                 print(3,end = "")
-            elif (seed + r) % 7 == 3:
+            elif (seed + r) % 25 == 3 or (seed + r) % 25 == 9 or (seed + r) % 25 == 15 or (seed + r) % 25 == 18 or (seed + r) % 25 == 21 or (seed + r) % 25 == 24:
                 map[flo][xx].next_route3 = -1
                 print(4,end = "")
-            elif (seed + r) % 7 == 4:
+            elif (seed + r) % 25 == 4 or (seed + r) % 25 == 10:
                 map[flo][xx].next_route1 = 1
                 map[flo][xx].next_route2 = 0
                 print(5,end = "")
-            elif (seed + r) % 7 == 5:
+            elif (seed + r) % 25 == 5 or (seed + r) % 25 == 11:
                 map[flo][xx].next_route2 = 0
                 map[flo][xx].next_route3 = -1
                 print(6,end = "")
-            elif (seed + r) % 7 == 6:
+            elif (seed + r) % 25 == 6 or (seed + r) % 25 == 12:
                 map[flo][xx].next_route3 = -1
                 map[flo][xx].next_route1 = 1
                 print(7,end = "")
